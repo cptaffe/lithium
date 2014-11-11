4e18b578baca31a1312843d1332836fde89ba800
Connor Taffe <cpaynetaffe@gmail.com>
2014-11-10 21:33:29 -0600

    command palette, io

--- lithium.cc
+++ lithium.cc
@@ -1,9 +1,9 @@
 #include "lithium.h"
-#include <gtkmm/box.h>
 #include <gtkmm/menubar.h>
 #include <gtkmm/menu.h>
 #include <gtkmm/textview.h>
 #include <gtkmm/scrolledwindow.h>
+#include <gtkmm/entry.h>
 #include <iostream>
 
 // window
@@ -37,41 +37,90 @@ namespace lith {
 				set_default_size(600, 800); // set window size
 				set_title("Lithium");
 
+				// set up keyboard shortcuts
+				add_events( Gdk::KEY_PRESS_MASK );
+				signal_key_press_event().connect ( sigc::mem_fun(*this, &window::on_key_press) );
+				signal_key_release_event().connect ( sigc::mem_fun(*this, &window::on_key_release) );
+
 				// vertical packing box
 				auto *vbox = Gtk::manage(new Gtk::Box(Gtk::ORIENTATION_VERTICAL, 0));
 				add(*vbox);
+				vbox->show();
 
 				// menu bar
 				auto*menubar = Gtk::manage(new Gtk::MenuBar());
 				vbox->pack_start(*menubar, Gtk::PACK_SHRINK, 0);
+				menubar->show();
 
-				// fill menu with items
+				// File menu
 				auto *menuitem_file = Gtk::manage(new Gtk::MenuItem("_File", true));
 				menubar->append(*menuitem_file);
+				menuitem_file->show();
 				auto *filemenu = Gtk::manage(new Gtk::Menu());
 				menuitem_file->set_submenu(*filemenu);
+				filemenu->show();
+
+				// File -> Open
+				auto *menuitem_open = Gtk::manage(new Gtk::MenuItem("_Open", true));
+				menuitem_open->signal_activate().connect(sigc::mem_fun(*this, &window::on_open_click));
+				filemenu->append(*menuitem_open);
+				menuitem_open->show();
+
+				// File -> Quit
 				auto *menuitem_quit = Gtk::manage(new Gtk::MenuItem("_Quit", true));
 				menuitem_quit->signal_activate().connect(sigc::mem_fun(*this, &window::on_quit_click));
 				filemenu->append(*menuitem_quit);
+				menuitem_quit->show();
+
+				// command interface
+				command = Gtk::manage(new commandPalette());
+				vbox->pack_start(*command, Gtk::PACK_SHRINK, 0);
 
 				// vertical packing box
 				auto *hbox = Gtk::manage(new Gtk::Box(Gtk::ORIENTATION_HORIZONTAL, 0));
 				vbox->pack_end(*hbox, Gtk::PACK_EXPAND_WIDGET, 0);
+				hbox->show();
 
 				// keeps track of editor
 				auto *editor_scroll = Gtk::manage(new Gtk::ScrolledWindow());
+				editor_scroll->set_kinetic_scrolling(true);
 				editor = Gtk::manage(new textView());
 				editor->set_name("editor_pane");
 				editor_scroll->add(*editor);
 				hbox->pack_start(*editor_scroll, Gtk::PACK_EXPAND_WIDGET, 0);
+				editor_scroll->show();
+				editor->show();
 
-				show_all();
+
+				show();
 			}
 
 			window::~window(){}
 
-			void window::on_button_click() {
+			void window::on_open_click() {
 				std::cout << "Hello World" << std::endl;
+			}
+
+			// do an action on a key press
+			bool window::on_key_press(GdkEventKey *event) {
+				if (event->keyval == GDK_KEY_Escape) {
+					hide();
+				}
+				return true;
+			}
+
+			// do an action on a key release
+			bool window::on_key_release(GdkEventKey *event) {
+				if ((event->state & GDK_CONTROL_MASK) && event->keyval == GDK_KEY_P) {
+					if (!command->is_visible()) {
+						command->show_all();
+						auto *txt = command->get_children()[0];
+						txt->grab_focus();
+					} else {
+						command->hide();
+					}
+				}
+				return true;
 			}
 
 			void window::on_quit_click() {
@@ -105,6 +154,23 @@ namespace lith {
 			treeView::treeView() {}
 
 			treeView::~treeView() {}
+
+		}
+	}
+}
+
+// editor file tree view
+namespace lith {
+	namespace ui {
+		namespace editor {
+
+			commandPalette::commandPalette() : Gtk::Box(Gtk::ORIENTATION_HORIZONTAL, 0) {
+				auto *command_text = Gtk::manage(new Gtk::Entry());
+				command_text->set_name("command");
+				pack_start(*command_text, Gtk::PACK_EXPAND_WIDGET, 0);
+			}
+
+			commandPalette::~commandPalette() {}
 
 		}
 	}
--- lithium.h
+++ lithium.h
@@ -7,8 +7,22 @@
 #include <gtkmm/cssprovider.h>
 #include <gtkmm/textview.h>
 #include <gtkmm/treeview.h>
+#include <gtkmm/box.h>
 
 namespace lith {
+
+	// namespace for lithium io
+	namespace io {
+		class sock {
+		public:
+			sock();
+			virtual ~sock();
+		protected:
+			// socket info
+		};
+	}
+
+	// namespace for lithium ui
 	namespace ui {
 
 		class window : public Gtk::Window {
@@ -37,18 +51,27 @@ namespace lith {
 				virtual ~textView();
 			};
 
+			class commandPalette : public Gtk::Box {
+			public:
+				commandPalette();
+				virtual ~commandPalette();
+			};
+
 			class window : public lith::ui::window {
 			public:
 				window(std::basic_string<char>);
 				virtual ~window();
 			protected:
 				// Signal handlers
-				void on_button_click();
+				bool on_key_press(GdkEventKey *);
+				bool on_key_release(GdkEventKey *);
+				void on_open_click();
 				void on_quit_click();
 
 				// editor view
 				textView *editor;
 				treeView *fview;
+				commandPalette *command;
 			};
 		}
 	}
